b8fe233a18b5439374b5a6fe893f4d50e9f8caef
akash.hiwse <akash.hiwse@YPH1015869LT.Yash.com>
2022-09-15 15:44:12 +0530

    Java8 program

---
+++ Java8_StreamAPI/src/com/yash/java8/p3/Check.java
@@ -0,0 +1,5 @@
+package com.yash.java8.p3;
+
+public interface Check {
+    void print();
+}
---
+++ Java8_StreamAPI/src/com/yash/java8/p6/PatternInterface.java
@@ -0,0 +1,6 @@
+package com.yash.java8.p6;
+
+public interface PatternInterface {
+    
+    void displayPattern(int row);
+}
